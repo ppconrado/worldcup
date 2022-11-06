785379a0ba7f0a4fc89905ef3b8be3dfc5d91fd8
ppconrado <ppconrado@yahoo.com.br>
2022-11-06 17:17:56 -0300

    web main page created

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,23 +1,85 @@
-interface HomeProps {
-  count: number;
-}
+// interface HomeProps {
+//   count: number;
+// }
+import Image from "next/image";
+import appPreviewImg from "../assets/app-nlw-copa-preview.png";
+import logoImg from "../assets/logo.svg";
+import usersAvatarExampleImg from "../assets/users-avatar-example.png";
+import IconCheckImg from "../assets/icon-check.svg";
 
-export default function Home(props: HomeProps) {
+export default function Home() {
   return (
-    <h1 className="text-violet-500 font-bold text-4xl">
-      Contagem: {props.count}
-    </h1>
+    <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 items-center gap-28">
+      <main>
+        <Image src={logoImg} alt="Copa" />
+
+        <h1 className="mt-14 text-white text-5xl font-bold leading-tight">
+          Crie seu proprio bolao da copa e compartilhe entre amigos!
+        </h1>
+
+        <div className="mt-10 flex items-center gap-2">
+          <Image src={usersAvatarExampleImg} alt="" />
+          <strong className="text-gray-100 text-xl">
+            <span className="text-ignite-500">+12.592</span> pessoas ja estao
+            usando
+          </strong>
+        </div>
+
+        <form className="mt-10 flex gap-2">
+          <input
+            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm"
+            type="text"
+            required
+            placeholder="Qual nome do seu bolao?"
+          />
+          <button
+            className="px-6 py-4 rounded bg-yellow-500 text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700"
+            type="submit"
+          >
+            Criar meu bolao
+          </button>
+        </form>
+
+        <p className="mt-4 text-sm text-gray-300 leading-relaxed">
+          Após criar seu bolão, você receberá um código único que poderá usar
+          para convidar outras pessoas 🚀
+        </p>
+
+        <div className="mt-10 pt-10 border-t border-gray-600 flex items-center justify-between text-gray-100">
+          {/*  */}
+          <div className="flex items-center gap-6">
+            <Image src={IconCheckImg} alt="" />
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+2.034</span>
+              <span>Boloes criados</span>
+            </div>
+          </div>
+
+          <div className="w-px h-14 bg-gray-600"></div>
+
+          <div className="flex items-center gap-6">
+            <Image src={IconCheckImg} alt="" />
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+192.847</span>
+              <span>Palpites enviados</span>
+            </div>
+          </div>
+          {/*  */}
+        </div>
+      </main>
+      <Image src={appPreviewImg} alt="Dois celulares exibindo" quality={100} />
+    </div>
   );
 }
 
-// JS desligado no NEXT
-export const getServerSideProps = async () => {
-  const response = await fetch("http://localhost:3333/pools/count");
-  const data = await response.json();
-  console.log(data);
-  return {
-    props: {
-      count: data.count,
-    },
-  };
-};
+// // JS desligado no NEXT
+// export const getServerSideProps = async () => {
+//   const response = await fetch("http://localhost:3333/pools/count");
+//   const data = await response.json();
+//   console.log(data);
+//   return {
+//     props: {
+//       count: data.count,
+//     },
+//   };
+// };
