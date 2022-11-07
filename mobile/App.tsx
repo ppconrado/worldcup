0d9373ed375503075f5d89be3383e08cb0efe1e7
ppconrado <ppconrado@yahoo.com.br>
2022-11-07 00:34:51 -0300

    context, useAuth and share data

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { NativeBaseProvider, StatusBar } from "native-base";
 import {
   useFonts,
@@ -7,11 +6,14 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
-import { Loading } from "./src/components/Loading";
-import { SignIn } from "./src/screens/Signin";
-import { THEME } from "./src/styles/theme";
+import { Pools } from "./src/screens/Pools";
 
-// import { StyleSheet, View } from "react-native";
+import { Loading } from "./src/components/Loading";
+
+import { THEME } from "./src/styles/theme";
+import { AuthContextProvider } from "./src/contexts/AuthContext";
+import { Routes } from "./src/routes";
+import React from "react";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,27 +21,17 @@ export default function App() {
     Roboto_500Medium,
     Roboto_700Bold,
   });
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#000",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-//   title: {
-//     color: "#fff",
-//     fontSize: 24,
-//   },
-// });
